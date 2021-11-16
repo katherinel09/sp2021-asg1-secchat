cbf7bcdae43e6b837687d53075d1f408f204f57c
Mark Wiering <markwiering@gmail.com>
2021-11-16 21:03:33 +0100

    Werkend gemaakt
    Ik heb api.h werkend gemaakt; anders compileert het kletsprogramma niet meer!

--- sp2021-asg1-secchat/framework/api.h
+++ sp2021-asg1-secchat/framework/api.h
@@ -1,13 +1,24 @@
 #ifndef _API_H_
 #define _API_H_
 
+#include <stdbool.h>
+
 struct api_msg {
-  /* TODO add information about message */
+  char* message;
 };
 
 struct api_state {
   int fd;
+  char* username;
+  char* input;
+  
   /* TODO add required fields */
+};
+
+
+struct argument{
+    int length;
+    char* arg;
 };
 
 
@@ -16,6 +27,7 @@ void api_recv_free(struct api_msg *msg);
 
 void api_state_free(struct api_state *state);
 void api_state_init(struct api_state *state, int fd);
+
 
 /* TODO add API calls to send messages to perform client-server interactions */
 
