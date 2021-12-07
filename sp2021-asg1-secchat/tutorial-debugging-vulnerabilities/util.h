83730ac8e1870234edb8a566f2a072a24158bd5a
Katherine Lasonde <Katherine.f.lasonde.23@dartmouth.edu>
2021-12-07 21:29:04 +0100

    Delete sp2021-asg1-secchat/tutorial-debugging-vulnerabilities directory

--- sp2021-asg1-secchat/tutorial-debugging-vulnerabilities/util.h
+++
@@ -1,9 +0,0 @@
-#ifndef __UTIL_H__
-size_t read_line(FILE *file, char *buf, size_t size);
-size_t read_line_nonewline(FILE *file, char *buf, size_t size);
-ssize_t safe_read(int fd, void *buf, size_t len);
-ssize_t safe_write(int fd, const void *buf, size_t len);
-void flush_print(char *string);
-#define __UTIL_H__
-
-#endif
